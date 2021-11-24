c6eb306149b25099e20888692d570a2ed44b8914
fanfanfufu <1127137365@qq.com>
2021-11-24 22:52:03 +0800

    第2章(1): 统一返回结果的封装对String类型的处理方式一
    该方式返回的结果, 将值为null的字段给处理掉了,并不理想

---
+++ src/main/java/cn/fuqi/springbootuselearning/config/ResponseAdvice.java
@@ -0,0 +1,37 @@
+package cn.fuqi.springbootuselearning.config;
+
+import cn.fuqi.springbootuselearning.common.ResultVo;
+import com.alibaba.fastjson.JSON;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author FuQi
+ * @date 2021/11/24 22:19
+ * @description
+ */
+@RestControllerAdvice
+public class ResponseAdvice implements ResponseBodyAdvice {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof ResultVo<?>) {
+            return body;
+        }
+        // 单独对String类型的返回数据进行处理方式一
+        if (body instanceof String) {
+            return JSON.toJSONString(new ResultVo<>(body));
+        }
+
+        return new ResultVo<>(body);
+    }
+}
